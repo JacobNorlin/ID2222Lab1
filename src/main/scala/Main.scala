9302349e71b1a3b13824566e61515e3eb2292055
JacobNorlin <jacob.norlinandersson@gmail.com>
2016-11-07 13:22:59 +0100

    Added readme. Made project runnable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,8 +9,10 @@ import org.apache.spark.SparkConf
 object Main {
 
   def main(args: Array[String]) = {
-    val logFile = "./" // Should be some file on your system
-    val conf = new SparkConf().setAppName("Simple Application")
+    val logFile = "./README.MD" // Should be some file on your system
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .setMaster(("local"))
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
